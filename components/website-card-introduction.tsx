3e84b7e33ac70c02e695b6da7079088a7fb0e70c
Humble.X <Humble.Xiang@gmail.com>
2022-02-09 11:24:49 +0800

    perf: lighthouse add meta description

--- components/website-card-introduction.tsx
+++ components/website-card-introduction.tsx
@@ -7,6 +7,7 @@ const WebsiteCardIntroduction = ({ url, title, description, image, favicon }: An
     <>
       <Head>
         <title>{title}</title>
+        <meta name="description" content={description}></meta>
       </Head>
       <Center>
         <LinkBox>
