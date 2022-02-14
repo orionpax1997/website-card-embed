aaa4eb3a852c8e3ec6f31154bd566c74ace1eb3c
Humble.X <Humble.Xiang@gmail.com>
2022-02-14 20:40:36 +0800

    feat: 支持根据系统切换暗色主题

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,22 @@ import useSWR, { Fetcher } from 'swr';
 import { AnalysisData } from '@humble.xiang/website-description-analysis';
 import WebsiteCardIntroductionLoading from '@/components/website-card-introduction-loading';
 import WebsiteCardIntroduction from '@/components/website-card-introduction';
+import { ColorModeScript } from '@chakra-ui/react';
 
 function AnalysisPage() {
   const url = useRouter().query.url as string;
+  const colorMode = useRouter().query.colorMode as string;
   const { isLoading, analysisData } = useAnalysisData(url);
-  if (isLoading) return WebsiteCardIntroductionLoading();
-  if (analysisData) return WebsiteCardIntroduction(analysisData);
-  return WebsiteCardIntroductionLoading();
+  return (
+    <>
+      <ColorModeScript initialColorMode={colorMode === 'dark' ? 'dark' : colorMode === 'light' ? 'light' : 'system'} />
+      {isLoading
+        ? WebsiteCardIntroductionLoading()
+        : analysisData
+        ? WebsiteCardIntroduction(analysisData)
+        : WebsiteCardIntroductionLoading()}
+    </>
+  );
 }
 
 const analysisDataFetcher: Fetcher<AnalysisData, string> = url => fetch(url).then(res => res.json());
---
+++ public/website-card-embed.js
@@ -0,0 +1,45 @@
+/**
+ * 获取渲染颜色模式的方法，基于现有的主题自定义
+ * @returns {string} 'dark' or 'light' or 'system'
+ */
+function getWebsiteCardEmbedColorMode() {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark';
+  if (window.matchMedia('(prefers-color-scheme: light)').matches) return 'light';
+  return 'system';
+}
+window.getWebsiteCardEmbedColorMode = getWebsiteCardEmbedColorMode;
+
+(function () {
+  var ie = !!(window.attachEvent && !window.opera),
+    wk = /webkit\/(\d+)/i.test(navigator.userAgent) && RegExp.$1 < 525;
+  var fn = [],
+    run = function () {
+      for (var i = 0; i < fn.length; i++) fn[i]();
+    },
+    d = document;
+  d.ready = function (f) {
+    if (!ie && !wk && d.addEventListener) {
+      return d.addEventListener('DOMContentLoaded', f, false);
+    }
+    if (fn.push(f) > 1) return;
+    if (ie)
+      (function () {
+        try {
+          d.documentElement.doScroll('left');
+          run();
+        } catch (err) {
+          setTimeout(arguments.callee, 0);
+        }
+      })();
+    else if (wk)
+      var t = setInterval(function () {
+        if (/^(loaded|complete)$/.test(d.readyState)) clearInterval(t), run();
+      }, 0);
+  };
+})();
+
+document.ready(function () {
+  Array.from(document.getElementsByClassName('website-card-embed')).forEach(function (item) {
+    item.src = item.getAttribute('data-src') + '&colorMode=' + window.getWebsiteCardEmbedColorMode();
+  });
+});
