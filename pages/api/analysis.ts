2acca7a8534f1b0ec60be1dc5b58c78f76a60f00
Humble.X <Humble.Xiang@gmail.com>
2022-02-15 15:58:48 +0800

    feat: 解析数据api基于url缓存

--- pages/api/analysis.ts
+++ pages/api/analysis.ts
@@ -1,8 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import NodeCache from 'node-cache';
 import AnalysisFactory from '@humble.xiang/website-description-analysis';
 
+const analysisDataCache = new NodeCache({ stdTTL: 60 * 60 * 24 * 3 });
+
 const analysis = async (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json((await AnalysisFactory.create(_.query.url as string)).analysis());
+  if (analysisDataCache.get(_.query.url as string)) {
+    res.status(200).json(analysisDataCache.get(_.query.url as string));
+  } else {
+    const analysisData = (await AnalysisFactory.create(_.query.url as string)).analysis();
+    analysisDataCache.set(_.query.url as string, analysisData);
+    res.status(200).json(analysisData);
+  }
 };
 
 export default analysis;
