f3f6944bb5c83aaec5ded7a7cba7f9ac20a6fa3c
Humble.X <Humble.Xiang@gmail.com>
2022-02-21 16:25:10 +0800

    refactor: 接口缓存方式重构

--- pages/api/analysis.ts
+++ pages/api/analysis.ts
@@ -1,18 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import NodeCache from 'node-cache';
 import AnalysisFactory from '@humble.xiang/website-description-analysis';
 
-const analysisDataCache = new NodeCache({ stdTTL: 60 * 60 * 24 * 3 });
-
 const analysis = async (_: NextApiRequest, res: NextApiResponse) => {
-  if (analysisDataCache.get(_.query.url as string)) {
-    res.status(200).json(analysisDataCache.get(_.query.url as string));
-  } else {
-    const analysisData = (await AnalysisFactory.create(_.query.url as string)).analysis();
-    analysisDataCache.set(_.query.url as string, analysisData);
-    console.info(`set ${_.query.url} to cache`);
-    res.status(200).json(analysisData);
-  }
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
+  res.status(200).json((await AnalysisFactory.create(_.query.url as string)).analysis());
 };
 
 export default analysis;
